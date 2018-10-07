232e86ef79f57508d6676cdc6d6c58f545f61603
espen <espenk95@gmail.com>
2018-10-07 17:29:55 +0200

    Add timeit decorator to log execution time of functions.

--- utils/decorator.py
+++ utils/decorator.py
@@ -1,6 +1,7 @@
 import logging
 import functools
 import inspect
+import datetime
 
 
 log = logging.getLogger("discord")
@@ -25,5 +26,31 @@ def logger(func):
             log.debug(f'{func.__name__} called with args {args}, kwargs {kwargs}')
             result = func(*args, **kwargs)
             log.debug(f'{func.__name__} returns {result}')
+            return result
+    return wrapped
+
+
+def timeit(func):
+    """Decorator to log function name execution time.
+
+    :param func: The function to decorate.
+    :return: The wrapped function.
+    """
+    if inspect.iscoroutinefunction(func):
+        async def wrapped(*args, **kwargs):
+            start_time = datetime.datetime.now()
+            result = await func(*args, **kwargs)
+            end_time = datetime.datetime.now()
+            delta = end_time - start_time
+            log.debug(f'{func.__name__} executed in {delta.total_seconds() * 1000:.2f} milliseconds')
+            return result
+    else:
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            start_time = datetime.datetime.now()
+            result = func(*args, **kwargs)
+            end_time = datetime.datetime.now()
+            delta = end_time - start_time
+            log.debug(f'{func.__name__} executed in {delta.total_seconds() * 1000:.2f} milliseconds')
             return result
     return wrapped

6bca6e0ab803016ff4307a31d293c29458bbc724
espen <espenk95@gmail.com>
2018-10-07 17:35:18 +0200

    Add listening function that is always asking zKillboard redisQ for new killmails.

--- cogs/intel.py
+++ cogs/intel.py
@@ -23,3 +23,38 @@ from . import config
 
 
 log = logging.getLogger('discord')
+
+
+async def listen(bot: commands.Bot):
+    """Listen for new killmail to be available from the zKillboard redisQ.
+
+    When a new killmail is available, fetch it and process it.
+
+    :param bot: The discord bot.
+    :return: None.
+    """
+    await bot.wait_until_ready()
+
+    url = 'https://redisq.zkillboard.com/listen.php?queueID=kverna'
+
+    timeout = aiohttp.ClientTimeout(total=20)
+    async with aiohttp.ClientSession(timeout=timeout, headers={"User-Agent": "kverna"}) as session:
+        while True:  # TODO: Check connection status
+            log.debug('listening')
+            zkb_data = await fetch(session=session, url=url)
+
+            if zkb_data is not None and zkb_data.get('package') is not None:
+                zkb = from_dict(cls=Zkb, dictionary=zkb_data.get('package').get('zkb'))
+
+                await asyncio.sleep(0.0001)
+
+                killmail_data = await fetch(session=session, url=zkb.href)
+
+                if killmail_data is not None:
+                    killmail = from_dict(cls=Killmail, dictionary=killmail_data)
+
+                    await asyncio.sleep(0.0001)
+
+                    await asyncio.sleep(2)
+            else:
+                await asyncio.sleep(2)

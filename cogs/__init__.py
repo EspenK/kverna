f2249f72d48cbd5ef00de859fa6bd7482ac216af
espen <espenk95@gmail.com>
2018-10-07 21:23:06 +0200

    Make a aiohttp.ClientSession object that can be imported and used by several modules.

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,6 +1,10 @@
 import asyncio
+import aiohttp
 from utils.file import load
 
 
 loop = asyncio.get_event_loop()
 config = loop.run_until_complete(load())
+
+headers = {"User-Agent": "kverna"}
+session = aiohttp.ClientSession(headers=headers, loop=loop)

5d9a178df21a45dea1e6d976d3556c4c1640749d
espen <espenk95@gmail.com>
2018-10-07 21:24:33 +0200

    Import and use the session object from __init__.py

--- cogs/intel.py
+++ cogs/intel.py
@@ -20,6 +20,7 @@ from utils.dataclass import Position
 from utils.file import load
 from utils.file import save
 from . import config
+from . import session
 
 
 log = logging.getLogger('discord')
@@ -37,27 +38,25 @@ async def listen(bot: commands.Bot):
 
     url = 'https://redisq.zkillboard.com/listen.php?queueID=kverna'
 
-    timeout = aiohttp.ClientTimeout(total=20)
-    async with aiohttp.ClientSession(timeout=timeout, headers={"User-Agent": "kverna"}) as session:
-        while True:  # TODO: Check connection status
-            log.debug('listening')
-            zkb_data = await fetch(session=session, url=url)
+    while True:  # TODO: Check connection status
+        log.debug('listening')
+        zkb_data = await fetch(session=session, url=url)
 
-            if zkb_data is not None and zkb_data.get('package') is not None:
-                zkb = from_dict(cls=Zkb, dictionary=zkb_data.get('package').get('zkb'))
+        if zkb_data is not None and zkb_data.get('package') is not None:
+            zkb = from_dict(cls=Zkb, dictionary=zkb_data.get('package').get('zkb'))
+
+            await asyncio.sleep(0.0001)
+
+            killmail_data = await fetch(session=session, url=zkb.href)
+
+            if killmail_data is not None:
+                killmail = from_dict(cls=Killmail, dictionary=killmail_data)
 
                 await asyncio.sleep(0.0001)
 
-                killmail_data = await fetch(session=session, url=zkb.href)
-
-                if killmail_data is not None:
-                    killmail = from_dict(cls=Killmail, dictionary=killmail_data)
-
-                    await asyncio.sleep(0.0001)
-
-                    asyncio.create_task(process_killmail(zkb=zkb, killmail=killmail, bot=bot))
-            else:
-                await asyncio.sleep(2)
+                asyncio.create_task(process_killmail(zkb=zkb, killmail=killmail, bot=bot))
+        else:
+            await asyncio.sleep(2)
 
 
 @timeit
